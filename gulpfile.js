bb62c50ad5e807236166f34238f8f1ef794eab04
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-02-16 16:54:10 +0800

    Add comments for purging css later

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const {src, dest, watch, series} = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+// const purgecss = require('gulp-purgecss')
 
-function buildStyles(){
+function buildStyles() {
   return src('sass/**/*.scss')
-    .pipe(sass())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    // .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
-function watchTask(){
-  watch(['sass/**/*.scss'], buildStyles)
+function watchTask() {
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
